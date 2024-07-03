349aa4e9d9c83a548d7d20a86255ce1126e8f27e
Kenward Terhemba <codewithkenward@gmail.com>
2024-07-03 15:19:48 +0100

    arithmetic operations function update

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,13 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 == 0:
-                return "You cannot do zero division"
-            else:
-                return num1 / num2
-        case _:
-            return "Invalid operation"
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "You cannot do a zero division"
+        return num1 / num2
+    else:
+        return "Invalid operation"
