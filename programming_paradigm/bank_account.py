59a2690b8f152c9c3613e40529d0774fa7243be4
Kenward Terhemba <codewithkenward@gmail.com>
2024-07-10 11:29:50 +0100

    Two Python scripts: bank_account.py, which contains the BankAccount class, and main-0.py, which interfaces with the class through command line arguments to perform banking operations update

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,17 @@
 class BankAccount:
     def __init__(self, initial_balance = 0):
-        self.__account_balance = initial_balance
+        self.__account_balance = int(initial_balance)
 
     def deposit(self, amount):
-        self.__account_balance += amount
+        self.__account_balance += int(amount)
 
     def withdraw(self, amount):
-        if amount <= self.__account_balance:
-            self.__account_balance -= amount
+        if int(amount) <= self.__account_balance:
+            self.__account_balance -= int(amount)
             return True
         else:
             return False
 
     def display_balance(self):
-        print(f"Current balance: ${self.__account_balance:.2f}")
+        print(f"Current Balance: ${self.__account_balance}")
 
