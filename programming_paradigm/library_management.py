966039b9946c4136f7fe194bd9e7a56d1c3c7683
Kenward Terhemba <codewithkenward@gmail.com>
2024-07-10 13:07:33 +0100

    Implementing Basic OOP for a Library Management System

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,12 +4,12 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = False  
+        self._is_checked_out = False
 
 class Library:
     def __init__(self):
         self._books = []  
-    
+
     def add_book(self, book):
         self._books.append(book)
     
